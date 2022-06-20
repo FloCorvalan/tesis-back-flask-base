42ebfd00dea0bab20834fdda3c34a68e055bf1ec
FloCorvalan <florencia.corvalan@usach.cl>
2022-06-20 11:51:37 -0400

    intento 2

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,17 +2,17 @@
 Lines=$(cat req2.txt)
 for Line in $Lines
 do
-    conda skeleton pypi "$Line"
+    conda run -n myenv /bin/bash -c conda skeleton pypi "$Line"
 done 
 
 for Line in $Lines
 do
-    conda-build "$Line"
+    conda run -n myenv /bin/bash -c conda-build "$Line"
 done 
 
 for Line in $Lines
 do
-    conda install --use-local "$Line"
+    conda run -n myenv /bin/bash -c conda install --use-local "$Line"
 done 
 
 conda run --no-capture-output -n myenv python app.py
