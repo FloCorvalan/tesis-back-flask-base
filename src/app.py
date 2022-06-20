a1fb753f70c5d623b0f40b8229e08a0720734eb9
FloCorvalan <florencia.corvalan@usach.cl>
2022-06-19 20:02:45 -0400

    sin prints

--- src/app.py
+++ src/app.py
@@ -6,9 +6,7 @@ from apps import create_app
 if __name__ == "__main__":
     pwd = getcwd()
     dotenv_path = pwd + '/.env'
-    print(dotenv_path)
     load_dotenv(dotenv_path)
-    print(environ)
     config_mode = environ.get('CONFIG_MODE')
     app_port = environ.get('PORT')
     app, mongo = create_app(config_mode)
